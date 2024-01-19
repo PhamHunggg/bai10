ba314a9e5c9af887ec4008b7d59aa4b507220ca4
LAPTOP <LAPTOP@HNG>
2024-01-19 11:02:45 +0700

    Add project files.

---
+++ bai10/SanPham.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai10
+{
+    internal class SanPham
+    {
+        public int masanpham { get; set; }
+        public string tensanpham { get; set; }
+        public double dongia { get; set; }
+        public int soluong { get; set; }
+        public SanPham()
+        {
+
+        }
+
+        public SanPham(int masanpham, string tensanpham, double dongia, int soluong)
+        {
+            this.masanpham = masanpham;
+            this.tensanpham = tensanpham;
+            this.dongia = dongia;
+            this.soluong = soluong;
+        }
+
+        public double ThanhTien()
+        {
+            return dongia * soluong;
+        }
+        public void In()
+    
+
+    }
+}
---
+++ bai10/SanPhamBase.cs
@@ -0,0 +1,16 @@
+﻿namespace bai10
+{
+    internal class SanPhamBase
+    {
+        public double dongia { get; set; }
+        public int masanpham { get; set; }
+        public int soluong { get; set; }
+        public string tensanpham { get; set; }
+
+
+        public double ThanhTien()
+        {
+            return dongia * soluong;
+        }
+    }
+}
